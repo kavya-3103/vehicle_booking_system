6377d1b98803a0d14ef704fde36d470d7202d475
kavya <bonagiri.kavya.31@gmail.com>
2025-05-10 16:20:13 +0530

    2nd- created database and tables

---
+++ vehicle_backend/prisma/migrations/20250510104542_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "VehicleType" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "wheels" INTEGER NOT NULL,
+
+    CONSTRAINT "VehicleType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vehicle" (
+    "id" SERIAL NOT NULL,
+    "modelName" TEXT NOT NULL,
+    "vehicleTypeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Vehicle_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "vehicleId" INTEGER NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Booking_vehicleId_startDate_endDate_key" ON "Booking"("vehicleId", "startDate", "endDate");
+
+-- AddForeignKey
+ALTER TABLE "Vehicle" ADD CONSTRAINT "Vehicle_vehicleTypeId_fkey" FOREIGN KEY ("vehicleTypeId") REFERENCES "VehicleType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_vehicleId_fkey" FOREIGN KEY ("vehicleId") REFERENCES "Vehicle"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
